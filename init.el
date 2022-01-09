514afbf6a3d6f76ee6ebd108040e999e39ce93e2
Dusan Kovacevic <dusankovacevic95@gmail.com>
2022-01-09 22:05:17 +0100

    Enable solaire mode

--- init.el
+++ init.el
@@ -580,7 +580,9 @@ dump.")
 This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
-before packages are loaded.")
+before packages are loaded."
+(solaire-global-mode +1))
+
 
 
 ;; Do not write anything past this comment. This is where Emacs will
